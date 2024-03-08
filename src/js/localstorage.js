69b9f0b19d20cdf3101d2c2cdd7a47376c25a32a
FrYt4 <adrian.fryczkie@yahoo.com>
2024-03-08 19:09:06 +0100

    addToWatched

--- src/js/localstorage.js
+++ src/js/localstorage.js
@@ -1,2 +1,29 @@
-console.log('localstorage ok');
-aaaaaaaaaa
+import { fetchMovieDetails, fetchTrendingMovies, genresName , fetchSearchMovies } from './api';
+import renderGallery from './gallery';
+          // Dodanie przycisku "Watched" i "Add to watched"
+          // pożniej można to ewentualnie zastąpić jak powstana przyciski
+const watchedButton = document.createElement('button');
+        watchedButton.innerText = 'Watched';
+        watchedButton.addEventListener('click', () => addToWatchedMovies(movieDetails));
+        card.appendChild(watchedButton);
+
+const addToWatchedMovies = movieDetails => {
+     // Pobierz listę obejrzanych filmów z localStorage lub utwórz nową listę, jeśli nie istnieje
+    const watchedMovies = JSON.parse(localStorage.getItem('watchedMowies')) || [];
+    const existingMovie = watchedMovies.find(movie => movie.id === movieDetails.id);
+    if (!existingMovie) {
+        watchedMovies.push(movieDetails);
+        localStorage.setItem('watchedMovies', JSON.stringify(watchedMovies));
+    }
+};
+
+const displayWatchedMovies = () => {
+    // Pobierz listę obejrzanych filmów z localStorage
+    const watchedMovies = JSON.parse(localStorage.getItem('watchedMovies')) || [];
+
+    // Wyświetl listę obejrzanych filmów w dowolny sposób
+    console.log('Watched Movies:', watchedMovies);
+}
+
+export { displayWatchedMovies };
+displayWatchedMovies();
