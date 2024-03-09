be1b3269a27608a0bfe360acbe6dcbf4b31dba05
MarekLempart <kama.zakupy@gmail.com>
2024-03-09 09:33:12 +0100

    fix gallery i fix search

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -11,29 +11,70 @@ const renderGallery = async () => {
     // Znalezienie kontenera dla galerii filmów
     const galleryContainer = document.getElementById('gallery-container');
 
-    // Wyświetlenie filmów
-    galleryContainer.innerHTML = movies
-      .map(movie => {
-        // Utworzenie elementu karty filmu
-        const movieCard = `
-          <div class="movie-card" data-movie-id="${movie.id}">
-            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${
-          movie.title
-        }" class="movie-poster">
-            <div class="movie-details">
-              <p class="movie-title">${movie.title}</p>
-              <p class="movie-info">${getGenres(movie.genre_ids)} | ${movie.release_date.slice(
-          0,
-          4,
-        )}</p>
-            </div>
-          </div>
-        `;
-        return movieCard;
-      })
-      .join('');
+    // Sprawdzenie czy lista filmów nie jest pusta
+    if (movies.length > 0) {
+      // Wyświetlenie filmów
+      galleryContainer.innerHTML = movies
+        .map(movie => {
+          // Sprawdź czy plakat istnieje
+          const posterPath = movie.poster_path
+            ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+            : 'plakat_zastepczy.jpg';
 
-    // Dodanie obsługi zdarzenia kliknięcia dla każdej karty filmu
+          // Utworzenie elementu karty filmu
+          const movieCard = `
+            <div class="movie-card" data-movie-id="${movie.id}">
+              <img src="${posterPath}" alt="${movie.title}" class="movie-poster">
+              <div class="movie-details">
+                <p class="movie-title">${movie.title}</p>
+                <p class="movie-info">${getGenres(movie.genre_ids)} | ${movie.release_date.slice(
+            0,
+            4,
+          )}</p>
+              </div>
+            </div>
+          `;
+          return movieCard;
+        })
+        .join('');
+      // Ukrycie komunikatu o braku wyników, jeśli lista filmów nie jest pusta
+      const notResult = document.getElementById('not-result');
+      notResult.style.display = 'none';
+    } else {
+      // Jeśli lista filmów jest pusta, wyświetl komunikat
+      galleryContainer.innerHTML = '';
+      const notResult = document.getElementById('not-result');
+      notResult.style.display = 'block';
+    }
+
+    // // Wyświetlenie filmów
+    // galleryContainer.innerHTML = movies
+    //   .map(movie => {
+    //     // Sprawdź czy plakat istnieje
+    //     const posterPath = movie.poster_path
+    //       ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+    //       : 'plakat_zastepczy.jpg';
+
+    //     // Utworzenie elementu karty filmu
+    //     const movieCard = `
+    //       <div class="movie-card" data-movie-id="${movie.id}">
+    //         <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${
+    //       movie.title
+    //     }" class="movie-poster">
+    //         <div class="movie-details">
+    //           <p class="movie-title">${movie.title}</p>
+    //           <p class="movie-info">${getGenres(movie.genre_ids)} | ${movie.release_date.slice(
+    //       0,
+    //       4,
+    //     )}</p>
+    //         </div>
+    //       </div>
+    //     `;
+    //     return movieCard;
+    //   })
+    //   .join('');
+
+    // Obsługa zdarzenia kliknięcia dla każdej karty filmu
     const movieCards = document.querySelectorAll('.movie-card');
     movieCards.forEach(card => {
       card.addEventListener('click', async () => {
