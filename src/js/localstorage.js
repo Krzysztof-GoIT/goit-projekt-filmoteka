708235f29b6b13671601aa50c68e3b7aa9f3818d
Krzysztof-GoIT <145707752+Krzysztof-GoIT@users.noreply.github.com>
2024-03-09 22:08:25 +0100

    Fix gh-pages
    Stałozmienna

--- src/js/localstorage.js
+++ src/js/localstorage.js
@@ -1,7 +1,6 @@
-
 export const addToWatchedMovies = movieDetails => {
   // Pobierz listę obejrzanych filmów z localStorage lub utwórz nową listę, jeśli nie istnieje
-  const watchedMovies = JSON.parse(localStorage.getItem('watchedMovies')) || [];
+  let watchedMovies = JSON.parse(localStorage.getItem('watchedMovies')) || [];
   const isMovieAlreadyWatched = watchedMovies.find(movie => movie.id === movieDetails.id);
 
   //pobierz liste filmów z kolejki z local storage zeby ussunąć w razie wuz
@@ -15,8 +14,6 @@ export const addToWatchedMovies = movieDetails => {
     //usuwa filmy jeśli znadjują sie w kolejce
     queuedMovies = queuedMovies.filter(movie => movie.id !== movieDetails.id);
     localStorage.setItem('queuedMovies', JSON.stringify(queuedMovies));
-      
-    
   } else {
     console.log(`"${movieDetails.title}" is already in Watched Movies`);
   }
@@ -25,19 +22,18 @@ export const addToWatchedMovies = movieDetails => {
 export const addToQueue = movieDetails => {
   const queuedMovies = JSON.parse(localStorage.getItem('queuedMovies')) || [];
   const isMovieInQueue = queuedMovies.find(movie => movie.id === movieDetails.id);
-  
-    //pobierz liste filmów z obejrzanych z local storage zeby ussunąć w razie wu
-    const watchedMovies = JSON.parse(localStorage.getItem('watchedMovies')) || [];
-    if (!isMovieInQueue) {
-        queuedMovies.push(movieDetails);
-        localStorage.setItem('queuedMovies', JSON.stringify(queuedMovies));
-      console.log(`Added "${movieDetails.title}" to Queue `);
-      
-      //usuwa filmy dodane do obejrzanych
-      watchedMovies = watchedMovies.filter(movie => movie.id !== movieDetails.id);
-      localStorage.setItem('watchedMovies', JSON.stringify(watchedMovies));
 
-    } else {
-        console.log (`"${movieDetails.title}" is already in Queue`);
-    }
-}
+  //pobierz liste filmów z obejrzanych z local storage zeby ussunąć w razie wu
+  const watchedMovies = JSON.parse(localStorage.getItem('watchedMovies')) || [];
+  if (!isMovieInQueue) {
+    queuedMovies.push(movieDetails);
+    localStorage.setItem('queuedMovies', JSON.stringify(queuedMovies));
+    console.log(`Added "${movieDetails.title}" to Queue `);
+
+    //usuwa filmy dodane do obejrzanych
+    watchedMovies = watchedMovies.filter(movie => movie.id !== movieDetails.id);
+    localStorage.setItem('watchedMovies', JSON.stringify(watchedMovies));
+  } else {
+    console.log(`"${movieDetails.title}" is already in Queue`);
+  }
+};
