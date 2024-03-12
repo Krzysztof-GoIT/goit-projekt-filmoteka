17527eb042778a613ac2f49735e020a6970921f8
Krzysztof-GoIT <145707752+Krzysztof-GoIT@users.noreply.github.com>
2024-03-12 18:21:18 +0100

    scrollToTop by Marek

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -235,3 +235,24 @@ const openModal = movieData => {
     }
   };
 };
+
+//scrollToTop by Marek
+const scrollToTopButton = document.getElementById('scrollToTopButton');
+
+// Pokaż przycisk, gdy użytkownik przewinie stronę w dół
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 100) {
+    // Możesz dostosować wartość, aby przycisk pojawił się po przewinięciu o określoną liczbę pikseli
+    scrollToTopButton.style.display = 'block';
+  } else {
+    scrollToTopButton.style.display = 'none';
+  }
+});
+
+// Obsługa zdarzenia kliknięcia przycisku
+scrollToTopButton.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth', // Działa w większości nowoczesnych przeglądarek, aby przewijać płynnie
+  });
+});
