4bad81a1e5ecfec5f97c2367e8aa92c58583ddbd
FrYt4 <adrian.fryczkie@yahoo.com>
2024-03-14 17:19:32 +0100

    in progress

--- src/js/firebaseAuthorization.js
+++ src/js/firebaseAuthorization.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, getAuth } from "firebase/auth";
 import { getDatabase, ref, set } from "firebase/database";
+import { onAuthStateChanged } from "firebase/auth";
 
 // Firebase configuration
 const firebaseConfig = {
@@ -12,6 +13,7 @@ const firebaseConfig = {
   messagingSenderId: "951424949581",
   appId: "1:951424949581:web:272460fadddef8c6452b04",
   measurementId: "G-G1QWHQFCQD",
+  databaseURL: "https://filmoteka-auth-eb393.firebaseio.com"
 };
 
 // Initialize Firebase
@@ -19,17 +21,24 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const database = getDatabase();
+const logoutButton = document.getElementById("logout-button");
+
+
 
 function register() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
   const fullName = document.getElementById("full-name").value;
 
-  if (!validateEmail(email) || !validatePassword(password) || !validateField(fullName)) {
-    alert('Email, Password, or Full Name is incorrect!');
+  if (!validateInputs(email, password, fullName)) {
+    alert('Invalid input!');
     return;
   }
 
+  createUser(email, password, fullName);
+}
+
+function createUser(email, password, fullName) {
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       const user = userCredential.user;
@@ -39,15 +48,23 @@ function register() {
         fullName: fullName,
         lastLogin: Date.now()
       };
-      return set(ref(database, 'users/' + user.uid), userData);
+      return saveUserData(user.uid, userData);
     })
     .then(() => {
       alert('User Created!');
     })
     .catch((error) => {
-      const errorMessage = error.message;
-      alert(errorMessage);
+      handleAuthError(error);
     });
+}
+
+function saveUserData(uid, userData) {
+  const databaseRef = ref(database, 'users/' + uid);
+  return set(databaseRef, userData);
+}
+function handleAuthError(error) {
+  const errorMessage = error.message;
+  alert(errorMessage);
 }
 
 function logIn() {
@@ -83,12 +100,36 @@ function validateEmail(email) {
 }
 
 function validatePassword(password) {
-  return password.length >= 6;
+  const MIN_PASSWORD_LENGTH = 6;
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasLowerCase = /[a-z]/.test(password);
+  const hasNumber = /\d/.test(password);
+  const hasSpecialChar = /[@#$%^&+=]/.test(password);
+
+  return password.length >= MIN_PASSWORD_LENGTH && hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar;
 }
 
-function validateField(field) {
-  return field && field.trim().length > 0;
+function logout() {
+  signOut(auth)
+    .then(() => {
+      // Wylogowanie zakończone sukcesem
+      alert('Log Out Succesed');
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      alert(errorMessage);
+    });
 }
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // Jeśli użytkownik jest zalogowany, wyświetl przycisk wylogowania
+    logoutButton.style.display = "block";
+  } else {
+    // Jeśli użytkownik nie jest zalogowany, ukryj przycisk wylogowania
+    logoutButton.style.display = "none";
+  }
+});
 
+document.getElementById("logout-button").addEventListener("click", logout);
 document.getElementById("login-button").addEventListener("click", logIn);
 document.getElementById("register-button").addEventListener("click", register);
