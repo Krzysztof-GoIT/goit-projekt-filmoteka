73d04c1dcad7544d48d92860bdcb4eb25b58fa9a
AnitaPrzedwojewska <nisiek@wp.pl>
2024-03-17 18:07:29 +0100

    comments for api

--- src/js/api.js
+++ src/js/api.js
@@ -1,19 +1,21 @@
 import axios from 'axios';
 
+// stałe parametry połączenia z API The Movie Data Base
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = '2c31d985c0705d4cec824ff15c12500a';
 const AUTHORIZATION =
   'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYzMxZDk4NWMwNzA1ZDRjZWM4MjRmZjE1YzEyNTAwYSIsInN1YiI6IjY1ZTg0MWVjM2ZlMTYwMDE2MjVjZTAzZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5HPQhM5yRj3vRPDY3tSFgGEmeCi69HNN3M4_g94gH5c';
 
-let options = {
+const options = {
+  method: 'GET',
   headers: {
     accept: 'application/json',
     Authorization: AUTHORIZATION,
   },
 };
 
-// Genres
-// https://api.themoviedb.org/3/genre/movie/list?language=en
+// Genres, np. https://api.themoviedb.org/3/genre/movie/list?language=en
+// pobieranie listy gatunków filmów
 export async function fetchGenres() {
   const endpointUrl = 'genre/movie/list';
   const searchParams = new URLSearchParams({
@@ -26,26 +28,17 @@ export async function fetchGenres() {
   return response.data;
 }
 
-// Posters
-// https://image.tmdb.org/t/p/original/hu40Uxp9WtpL34jv3zyWLb5zEVY.jpg
+// Posters, np. https://image.tmdb.org/t/p/original/hu40Uxp9WtpL34jv3zyWLb5zEVY.jpg
+// pobieranie plakatu na podstawie adresu
 async function getPoster(posterUrl) {
-  options = {
-    method: 'GET',
-    // console.log('getPoster starts...');
-  };
   const url = `POSTERS_URL${posterUrl}`;
   const response = await axios(url, options);
-  // console.log('response: ', response);
   return response.data;
 }
 
-// Trending:
-// https://api.themoviedb.org/3/trending/movie/day?language=en-US
+// Trending, np. https://api.themoviedb.org/3/trending/movie/day?language=en-US
+// pobieranie listy popularnych filmów
 export async function fetchTrendingMovies(pageNo) {
-  options = {
-    method: 'GET',
-  };
-  // console.log('fetchPopularMovies starts...');
   const endpointUrl = 'trending/movie/day';
   const searchParams = new URLSearchParams({
     api_key: API_KEY,
@@ -54,17 +47,12 @@ export async function fetchTrendingMovies(pageNo) {
   });
   const url = `${BASE_URL}${endpointUrl}?${searchParams}`;
   const response = await axios(url, options);
-  // console.log('response: ', response);
   return response.data;
 }
 
-// Search
-// https://api.themoviedb.org/3/search/movie?query=avengers&include_adult=false&language=en-US&page=1
+// Search, np. https://api.themoviedb.org/3/search/movie?query=avengers&include_adult=false&language=en-US&page=1
+// pobieranie listy filmów wyszukanych na podstawie słów kluczowych
 export async function fetchSearchMovies(keywords, pageNo) {
-  options = {
-    method: 'GET',
-  };
-  // console.log('fetchSearchMovies starts...');
   const endpointUrl = 'search/movie';
   const searchParams = new URLSearchParams({
     api_key: API_KEY,
@@ -75,17 +63,12 @@ export async function fetchSearchMovies(keywords, pageNo) {
   });
   const url = `${BASE_URL}${endpointUrl}?${searchParams}`;
   const response = await axios(url, options);
-  // console.log('response: ', response);
   return response.data;
 }
 
-// Movie Details
-// https://api.themoviedb.org/3/movie/12345?language=en-US
+// Movie Details, np. https://api.themoviedb.org/3/movie/12345?language=en-US
+// pobieranie szczegółowych informacji o podanym filmie
 export async function fetchMovieDetails(movieId) {
-  options = {
-    method: 'GET',
-  };
-  // console.log('fetchMovieDetails starts...');
   const endpointUrl = 'movie';
   const searchParams = new URLSearchParams({
     api_key: API_KEY,
@@ -93,17 +76,12 @@ export async function fetchMovieDetails(movieId) {
   });
   const url = `${BASE_URL}${endpointUrl}/${movieId}?${searchParams}`;
   const response = await axios(url, options);
-  // console.log('response: ', response);
   return response.data;
 }
 
-// Movie Trailer
-// 'https://api.themoviedb.org/3/movie/123455/videos?language=en-US
+// Movie Trailer, np. 'https://api.themoviedb.org/3/movie/123455/videos?language=en-US
+// pobiernie listy trailerów dla podanego filmu
 export async function fetchMovieTrailers(movieId) {
-  options = {
-    method: 'GET',
-  };
-  // console.log('fetchMovieDetails starts...');
   const endpointUrl = 'movie';
   const searchParams = new URLSearchParams({
     api_key: API_KEY,
@@ -111,11 +89,11 @@ export async function fetchMovieTrailers(movieId) {
   });
   const url = `${BASE_URL}${endpointUrl}/${movieId}/videos?${searchParams}`;
   const response = await axios(url, options);
-  // console.log('response: ', response);
   return response.data;
 }
 
 // GENRES_LIST
+// zamiast częstego pobierania, gotowa lista gatunków filmów
 export const genresName = [
   { id: 28, name: 'Action' },
   { id: 12, name: 'Adventure' },
